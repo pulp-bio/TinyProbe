ba72cec3e3c7b24327a8ac012ce27c964ae261b8
vsergei <vsergei@iis.ee.ethz.ch>
2025-09-07 22:16:31 +0200

    Added licenses, added images, added READMEs

---
+++ CHANGELOG.md
@@ -0,0 +1,17 @@
+# Changelog
+
+All main changes to this project will be documented in this file.
+For the detailed description, please explore nested folders and corresponding CHANGELOG.md files (e.g. for PCB projects or firmware). 
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [0.1.0] - 2025-09-15
+
+### Added
+
+- Initial release of the Wifi 6 (WiUS) shield
+
+### Fixed
+
+### Changed
--- host_pc/tinyprobe/drivers/hal/fpga.py
+++ host_pc/tinyprobe/drivers/hal/fpga.py
@@ -1,3 +1,21 @@
+"""
+Copyright (C) 2025 ETH Zurich. All rights reserved.
+
+Author: Sergei Vostrikov, ETH Zurich
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 from tinyprobe.drivers.hal import TP_HAL
 from tinyprobe.drivers.ll.fpga_ll import TP_LL_FPGA
 from tinyprobe.protocol.commands import (
