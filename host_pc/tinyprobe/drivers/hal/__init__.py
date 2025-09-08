fa561491a2a71c42cc4aa1eaf2e8c4bbd139aaa0
vsergei <vsergei@iis.ee.ethz.ch>
2025-09-08 22:28:29 +0200

    Added individual license files, fixed host_pc script headers

--- host_pc/tinyprobe/drivers/hal/__init__.py
+++ host_pc/tinyprobe/drivers/hal/__init__.py
@@ -1,3 +1,23 @@
+"""
+    Copyright (C) 2025 ETH Zurich. All rights reserved.
+
+    Author: Cedric Hirschi, ETH Zurich
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+   SPDX-License-Identifier: Apache-2.0
+"""
+
 from tinyprobe.drivers.ll.fpga_ll import TP_LL_FPGA
 from tinyprobe.drivers.ll.afe5832lp_ll import TP_LL_AFE5832LP_GLB, TP_LL_AFE5832LP_DTGC
 from tinyprobe.drivers.ll.tx7332_ll import TP_LL_TX7332
